b6f44cf3ca426c9e4daf27badecfe873108af961
Jérémie Ledentu <jledentu@gmail.com>
2017-03-10 10:41:58 +0100

    Update dist files

--- dist/domEllipser.js
+++ dist/domEllipser.js
@@ -7,6 +7,7 @@
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.DomEllipser = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
+var DEFAULT_ELLIPSIS = '… ';
 var DomEllipser = (function () {
     function DomEllipser() {
         this.isAlreadyProcessed = function (domE) {
@@ -15,6 +16,10 @@ var DomEllipser = (function () {
     }
     DomEllipser.prototype.isAlreadyEllipsed = function (domE) {
         return !!domE.querySelector("[" + DomEllipser.DATA_ATTRIBUTES.ellipsis + "]");
+    };
+    DomEllipser.prototype.getOriginalText = function (domE) {
+        var originalE = domE.querySelector("[" + DomEllipser.DATA_ATTRIBUTES.original + "]");
+        return (originalE) ? originalE.textContent : null;
     };
     DomEllipser.prototype.getOriginalContent = function (domE) {
         var originalE = domE.querySelector("[" + DomEllipser.DATA_ATTRIBUTES.original + "]");
@@ -140,6 +145,7 @@ var DomEllipser = (function () {
         fragment.appendChild(originalE);
         var croppedE = document.createElement("span");
         croppedE.setAttribute(DomEllipser.DATA_ATTRIBUTES.cropped, "true");
+        croppedE.setAttribute("style", "white-space: normal;");
         fragment.appendChild(croppedE);
         var ellipsisE = document.createElement("span");
         ellipsisE.setAttribute(DomEllipser.DATA_ATTRIBUTES.ellipsis, "true");
@@ -147,7 +153,7 @@ var DomEllipser = (function () {
             ellipsisE.innerHTML = options.ellipsisHTML;
         }
         else {
-            ellipsisE.textContent = options.ellipsis || '…';
+            ellipsisE.textContent = options.ellipsis || DEFAULT_ELLIPSIS;
         }
         fragment.appendChild(ellipsisE);
         for (var child = wrapperE.firstChild; child; child = wrapperE.firstChild) {
